263505964080b69b2e48ea6f88d4467d52c20eaf
TajoRoland <scamget77@gmail.com>
2025-05-28 13:10:01 +0200

    807-es feladat

---
+++ XP feladatok/807/adatok.js
@@ -0,0 +1,14 @@
+const adatok = [
+  `"az";"nev";"anyjaneve";"telepules";"cim";"netto";"juttatas";"belepes";"szuletes";"szulhely"`,
+  `"16";"Fekete Zoltán";"Fehér Edit";"Tab";"Holdfény utca u.19";"630000";"10000";"2007-04-16";"1979-07-15";"Tab"`,
+  `"17";"Szabó Lajos";"Nagy Erzsébet";"Izsófalva";"Babits Mihály utca u.86";"210000";"5000";"2002-08-17";"1957-04-16";"Izsófalva"`,
+  `"18";"Erdős Dániel";"Erős Erzsébet";"Szany";"Gárdonyi Géza utca u.57";"700000";"0";"2003-10-12";"1981-11-07";"Dunaújváros"`,
+  `"19";"Fehér Dániel";"Erdős Katalin";"Gödöllő";"Madách Imre utca u.73";"440000";"10000";"2006-01-17";"1961-01-16";"Balatonfûzfő"`,
+  `"20";"Erős Lajos";"Gyenge Kitti";"Kerepes";"Janus Pannonius utca u.78";"690000";"10000";"1999-09-25";"1986-07-13";"Kerepes"`,
+  `"31";"Nagy Lajos";"Fekete Zsanett";"Balassagyarmat";"Baross utca u.96";"10000";"5000";"2008-03-22";"1986-03-17";"Szigetvár"`,
+  `"35";"Fehér Károly";"Gyenge Zsanett";"Őrbottyán";"Márkus Géza utca u.36";"300000";"10000";"2009-08-08";"1987-11-15";"Ajka"`,
+  `"36";"Almási László";"Almási Adrienn";"Nyírtelek";"Fadrusz János utca u.45";"450000";"0";"2007-09-26";"1954-06-21";"Tát"`,
+  `"37";"Kis Dániel";"Nagy Anna";"Gödöllő";"Csengery utca u.90";"730000";"0";"2006-06-02";"1984-07-10";"Gödöllő"`,
+  `"38";"Kiss Béla";"Erdős Hanna";"Marcali";"Gárdonyi Géza utca u.21";"150000";"0";"2006-02-19";"1970-03-17";"Szeghalom"`,
+  `"39";"Erdős György";"Erdős Laura";"Jánossomorja";"Kard utca u.64";"780000";"5000";"2008-08-20";"1983-10-08";"Bugyi"`
+];
